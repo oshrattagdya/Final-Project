19d8f074f1b96fd34bee0095b235e4899eb80629
oshrattagadya <oshrattagaya@gmail.com>
2022-06-09 11:59:19 +0300

    first commit - oshrat

---
+++ Managment/Base/BasePage.py
@@ -0,0 +1,25 @@
+import pytest
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class Base:
+
+    @pytest.fixture(autouse=True)
+    def set_up(self):
+        print("Initiating Chrome driver")
+        # self.driver = webdriver.Chrome('../Drivers/chromedriver.exe')
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        print("-----------------------------------------")
+        print("Test is started")
+        self.driver.implicitly_wait(10)
+        self.driver.get("https://qa-admin.trado.co.il/#/login")
+        self.driver.maximize_window()
+
+        yield self.driver
+
+        if self.driver is not None:
+            print("-----------------------------------------")
+            print("Tests is finished")
+            self.driver.close()
+            self.driver.quit()
---
+++ Managment/Locators/Login_locators.py
@@ -0,0 +1,9 @@
+from Managment.Base import BasePage
+
+""" Locators for login screen"""
+
+class Login_Locators:
+    phone_field = "form_formItem undefined   undefined  formItem_phone"
+    phone_code = "input_relative  "
+    send_code_button = "form_submitBtn"
+    login_button = "form_submitBtn"
