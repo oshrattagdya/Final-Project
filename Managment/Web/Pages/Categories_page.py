02c48c7b816f5a044bb8886572cd612d13273110
yalem <yalem8@gmail.com>
2022-06-13 21:42:27 +0300

    add comments to methods

--- Managment/Web/Pages/Categories_page.py
+++ Managment/Web/Pages/Categories_page.py
@@ -20,46 +20,47 @@ class CategoriesPageFunc():
         self.type_ops = CategoriesLocators.type_ops
         self.add_submit = CategoriesLocators.add_submit
         self.product_status_op = CategoriesLocators.product_status_op
-
+    # go to categories screen
     def click_categories_navbar(self):
         self.driver.find_element(By.XPATH,self.categories_nav_btn).click()
-
+    # click on 3 dots button to open options
     def click_options_navbar(self):
         self.driver.find_element(By.XPATH,self.options).click()
-
+    # click on add new category
     def click_add_btn(self):
         WebDriverWait(self.driver,10).until(
             EC.presence_of_element_located((By.XPATH,self.add_btn))
         ).click()
-
+    # final step to add new category click on add button on form
     def click_submit_add_category(self):
         self.driver.find_element(By.XPATH,self.add_submit).click()
-
+    # click on active option on form
     def click_status_active_op(self):
         self.driver.find_element(By.XPATH,self.product_status_op).click()
 
 
-
+    #search for specific category on serach box
     def search_category(self,categ_name):
         self.categ_name = categ_name
         search = self.driver.find_element(By.XPATH,self.search_categ_btn)
         search.send_keys(self.categ_name)
         search.send_keys(Keys.RETURN)
-
+    # insert new category name in add new category form
     def insert_new_category_name(self,new_categ_name):
         self.new_categ_name = new_categ_name
         self.driver.find_element(By.XPATH,self.name_field).send_keys(self.new_categ_name)
 
-
+    # insert new department name in add new category form
     def insert_department_name(self,department_name):
         self.department_name = department_name
         self.driver.find_element(By.XPATH,self.department_filed).send_keys(self.department_name)
 
-
+    # insert new field name in add new category form
     def insert_field_name(self,field_name):
         self.field_name = field_name
         self.driver.find_element(By.XPATH,self.fields_name).send_keys(self.field_name)
 
+    # select type option  in add new category form
     def type_option(self,op):
         self.driver.find_element(By.XPATH,self.type_field).click()
 
