1b272951cae35f04836591453653e085df5d0e9f
yalem <yalem8@gmail.com>
2022-06-14 14:32:18 +0300

    Base update

--- Managment/Web/Base/BasePage.py
+++ Managment/Web/Base/BasePage.py
@@ -3,6 +3,9 @@ from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.by import By
 
+from Managment.Web.Pages.Login_page import LoginPageFunc
+
+
 class Base:
 
     @pytest.fixture(autouse=True)
@@ -22,3 +25,15 @@ class Base:
             print("Tests is finished")
             self.driver.close()
             self.driver.quit()
+
+    @pytest.fixture
+    def connect_home_page(self):
+        driver = self.driver
+        login = LoginPageFunc(driver)
+        login.enter_phone('1950000000')
+        self.driver.implicitly_wait(10)
+        login.click_on_button()
+        self.driver.implicitly_wait(10)
+        login.enter_phone_code('1234')
+        login.click_on_button_login()
+        yield self.driver
