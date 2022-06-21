28b1003f415eab69002aa603725db49ba240a8e3
oshrattagadya <oshrattagaya@gmail.com>
2022-06-21 20:17:42 +0300

    changes oshrat

--- Managment/Web/Tests/Functional/Test_login.py
+++ Managment/Web/Tests/Functional/Test_login.py
@@ -6,13 +6,10 @@ from Managment.Web.Base.BasePage import Base
 from Managment.Web.Utils.utils import Utilitis
 from Managment.Web.Pages.Login_page import LoginPageFunc
 
-
-
-
 @pytest.mark.usefixtures('set_up')
 class TestLogin(Base):
-
     """test 1"""
+    @pytest.mark.sanity
     def test_login_success(self):
         driver = self.driver
         util = Utilitis(driver)
@@ -22,16 +19,13 @@ class TestLogin(Base):
         time.sleep(2)
         login.click_on_button()
         time.sleep(2)
-        driver.implicitly_wait(10)
-        time.sleep(2)
         login.enter_phone_code('1234')
         time.sleep(2)
         #clickin on login button
         login.click_on_button_login()
         time.sleep(2)
-
-        #using the assert function to valid our successful login
-        util.assertFunc(login.get_nameT1(),"שלום ולדימירהתנתק")
+        """assert"""
+        util.assertFunc(login.get_nameT1(),"שלום slamonnnהתנתק")
 
     """test 2"""
     def test_login_incorrectly_when_user_non_register(self):
@@ -40,17 +34,18 @@ class TestLogin(Base):
         login = LoginPageFunc(driver)
         login.enter_phone('4528762424')
         login.click_on_button()
-
+        """assert"""
         util.assertFunc(login.get_nameT2(),"no such user")
 
     """test 3"""
+    @pytest.mark.skip
     def test_login_incorrect_when_phone_field_null(self):
         driver = self.driver
         util = Utilitis(driver)
         login = LoginPageFunc(driver)
-        login.enter_phone('')
+        # login.enter_phone()
         login.click_on_button()
-
+        """assert"""
         util.assertFunc(util.valid_Message,'זהו שדה חובה.')
 
     """test 4"""
@@ -60,13 +55,11 @@ class TestLogin(Base):
         login = LoginPageFunc(driver)
         login.enter_phone('45')
         login.click_on_button()
-
         try:
             value = driver.find_element(By.XPATH, user).get_attribute("innerText")
             assert user == "no such user"
         except Exception as e:
             driver.get_screenshot_as_png()
-
 
     """test 5"""
     def test_login_incorrectly_when_code_is_not_match(self):
@@ -93,7 +86,7 @@ class TestLogin(Base):
         login.click_on_button_login()
         time.sleep(1)
         util = Utilitis(driver)
-        x = util.JS_Message("input[placeholder='קוד']")
+        x = util.valid_Message("input[placeholder='קוד']")
         try :
             assert x == 'זהו שדה חובה.'
         except Exception as e:
